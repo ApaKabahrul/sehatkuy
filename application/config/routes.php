36b4794a69374053ebf9e109c871b69b417a55fe
Bahrul Faizi <37656213+ApaKabahrul@users.noreply.github.com>
2018-11-20 20:09:48 +0700

    Add files via upload

--- application/config/routes.php
+++ application/config/routes.php
@@ -5,6 +5,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'home';
 $route['agadmigin'] = 'login/agadmigin';
 	$route['edit_obat'] = 'admin/edit_obat';
+	$route['edit_penyakit'] = 'admin/edit_penyakit';
+	$route['edit_lokasi'] = 'admin/edit_lokasi';
 $route['obat'] = 'home/data_obat';
 $route['sehat'] = 'home/sehat';
 	$route['manfaat_olahraga'] = 'home/manfaat_olahraga';
