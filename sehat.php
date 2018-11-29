6b05cb05e6cc9762a105b0da6c49e23583fc4bc4
osafaridasari <37997955+osafaridasari@users.noreply.github.com>
2018-11-29 15:02:48 +0700

    Add files via upload

---
+++ sehat.php
@@ -0,0 +1,226 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Site Title -->
+    <title>Sehat Kuy</title>
+    <!-- Meta Description Tag -->
+    <meta name="Description" content="Sehat Kuy">
+    <!-- Favicon Icon -->
+    <link rel="icon" type="image/x-icon" href="images/favicon.png" />
+    <!-- Font Awesoeme Stylesheet CSS -->
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" />
+    <!-- Google web Font -->
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600">
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Material Design Lite Stylesheet CSS -->
+    <link rel="stylesheet" href="css/material.min.css" />
+    <!-- Material Design Select Field Stylesheet CSS -->
+    <link rel="stylesheet" href="css/mdl-selectfield.min.css">
+    <!-- Owl Carousel Stylesheet CSS -->
+    <link rel="stylesheet" href="css/owl.carousel.min.css" />
+    <!-- Animate Stylesheet CSS -->
+    <link rel="stylesheet" href="css/animate.min.css" />
+    <!-- Magnific Popup Stylesheet CSS -->
+    <link rel="stylesheet" href="css/magnific-popup.css" />
+    <!-- Flex Slider Stylesheet CSS -->
+    <link rel="stylesheet" href="css/flexslider.css" />
+    <!-- Custom Main Stylesheet CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/sehatkuy.css">
+</head>
+<body style="background-color: #f9dbf0">
+<body>
+    
+    <header id="header">
+        
+        <div id="hdr-wrapper">
+            <div class="layer-stretch hdr">
+                <div class="tbl">
+                    <div class="tbl-row">
+                        
+                        <div class="tbl-cell hdr-logo">
+                            <a href="index.html"><img src="images/logo.png" alt=""></a>
+                        </div>
+                        <div class="tbl-cell hdr-menu">
+                            
+                            <ul class="menu">
+<li><a href="/codeigniter">Home</a></li>
+  <li><a href="/codeigniter/obat">Data Obat</a></li>
+  <li><a class="active" href="/codeigniter/sehat">Hidup Sehat</a></li>
+  <li><a href="/codeigniter/penyakit">Data Penyakit</a></li>
+  <li><a href="/codeigniter/lokasi">Lokasi Rumah Sakit</a></li>
+  <li><a href="/codeigniter/tentang">Tentang</a></li>
+                            
+                    </ul>
+                       </div>
+            </div>
+        </div>
+    </header>
+   <div class="page-ttl">
+        <div class="layer-stretch">
+            <div class="page-ttl-container">
+                <h1>Artikel</h1>
+                <p><a href="/codeigniter">Home</a> &#8594; <span>Daftar Artikel</span></p>
+            </div>
+        </div>
+    </div>
+
+     <div class="layer-stretch">
+        <div class="layer-wrapper text-center">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="blog-list-block">
+                        <div class="blog-list-picture">
+                            <div class="theme-img theme-img-scalerotate">
+                                <img src="images/post1.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="blog-list-ttl">
+                            <h3><a href="/codeigniter/manfaat_olahraga"> Manfaat Olahraga Bagi Kesehatan Mental</a></h3>
+                        </div>
+                        <div class="blog-list-meta">
+                            <p>Posted By On 24 Okt 2018</p>
+                        </div>  
+                        <div class="blog-list-post">
+                            <p class="paragraph-medium paragraph-black">
+                                <span>Semua orang tahu bahwa manfaat olahraga yang utama adalah membuat tubuh lebih sehat. 
+                                        Selain tubuh yang sehat,tahukah... </span>
+                                <a href="/codeigniter/manfaat_olahraga">(Read More)</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="blog-list-block">
+                        <div class="blog-list-picture">
+                            <div class="theme-img theme-img-scalerotate">
+                                <img src="images/post3.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="blog-list-ttl">
+                            <h3><a href="/codeigniter/kebiasaan_hidup_sehat"> Kebiasaan Hidup Sehat yang Baik Dilakukan Sebelum Tidur Malam </a></h3>
+                        </div>
+                        <div class="blog-list-meta">
+                            <p>Posted On 24 Okt 2018</p>
+                        </div>  
+                        <div class="blog-list-post">
+                            <p class="paragraph-medium paragraph-black">
+                                <span>Kebiasaan hidup sehat memang bisa dilakukan di mana pun dan kapanpun, bisa dilakukan di sela-sela 
+                waktu luang. Tapi tahukah kamu,..</span>
+                                <a href="/codeigniter/kebiasaan_hidup_sehat">(Read More)</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="blog-list-block">
+                        <div class="blog-list-picture">
+                            <div class="theme-img theme-img-scalerotate">
+                                <img src="images/post5.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="blog-list-ttl">
+                           <h3><a href="/codeigniter/cegah_penyakit_jantung"> Cegah diri Dari Penyakit Jantung</a></h3>
+                        </div>
+                        <div class="blog-list-meta">
+                            <p>Posted On 24 Okt 2018</p>
+                        </div>  
+                        <div class="blog-list-post">
+                            <p class="paragraph-medium paragraph-black">
+                                <span>Angka kematian akibat sakit jantung terus meningkat dibandingkan tahun-tahun sebelumnya. Salah satu hal yang turut 
+                menyumbang peningkatan sakit ... </span>
+                                <a href="/codeigniter/cegah_penyakit_jantung">(Read More)</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-md-6">
+                    <div class="blog-list-block">
+                        <div class="blog-list-picture">
+                            <div class="theme-img theme-img-scalerotate">
+                                <img src="images/post2.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="blog-list-ttl">
+                           <h3><a href="/codeigniter/tips_pola_sehat"> 7 Tips Pola Hidup Sehat yang Harus Mulai Dilakukan dari Sekarang</a></h3>
+                        </div>
+                        <div class="blog-list-meta">
+                            <p>Posted On 25 Okt 2018</p>
+                        </div>  
+                        <div class="blog-list-post">
+                            <p class="paragraph-medium paragraph-black">
+                                <span>Dalam kesehatan ada adagium, kesehatan bukanlah segalanya, namun segalanya tanpa kesehatan bukanlah apa-apa.Health is not everything, but ... </span>
+                                <a href="/codeigniter/tips_pola_sehat">(Read More)</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                
+            
+        </div>
+    </div>
+
+ 
+</div>
+    <!-- **********Included Scripts*********** -->
+
+    <!-- Jquery Library 2.1 JavaScript-->
+    <script src="js/jquery-2.1.4.min.js"></script>
+    <!-- Popper JavaScript-->
+    <script src="js/popper.min.js"></script>
+    <!-- Bootstrap Core JavaScript-->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Material Design Lite JavaScript-->
+    <script src="js/material.min.js"></script>
+    <!-- Material Select Field Script -->
+    <script src="js/mdl-selectfield.min.js"></script>
+    <!-- Flexslider Plugin JavaScript-->
+    <script src="js/jquery.flexslider.min.js"></script>
+    <!-- Owl Carousel Plugin JavaScript-->
+    <script src="js/owl.carousel.min.js"></script>
+    <!-- Scrolltofixed Plugin JavaScript-->
+    <script src="js/jquery-scrolltofixed.min.js"></script>
+    <!-- Magnific Popup Plugin JavaScript-->
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <!-- WayPoint Plugin JavaScript-->
+    <script src="js/jquery.waypoints.min.js"></script>
+    <!-- CounterUp Plugin JavaScript-->
+    <script src="js/jquery.counterup.js"></script>
+    <!-- SmoothScroll Plugin JavaScript-->
+    <script src="js/smoothscroll.min.js"></script>
+    <!--Custom JavaScript for Klinik Template-->
+    <script src="js/custom.js"></script>
+    <!-- Google Map Block Script -->
+    <script>
+        var map;
+        function initMap() {
+            var loc = {
+                lat: YOUR_LATTITUDE,
+                lng: YOUR_LONGITUDE 
+            };
+            var isDraggable = !('ontouchstart' in document.documentElement);
+
+            map = new google.maps.Map(document.getElementById('map-container'), {
+                center: loc,
+                zoom: 14,
+                draggable: isDraggable,
+                scrollwheel: false
+            });
+
+            var marker = new google.maps.Marker({
+                position: loc,
+                map: map
+            });
+        }
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&amp;callback=initMap" async defer></script>
+</body>
+</html>
+
