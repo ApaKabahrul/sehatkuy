63fb8d155c3a6d8f052c5859120d1804ed1b8308
Angelamerici <38027594+Angelamerici@users.noreply.github.com>
2018-11-29 23:04:14 +0700

    Add files via upload

--- application/views/tentang.php
+++ application/views/tentang.php
@@ -2,76 +2,190 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
-<title> Tentang SEHAT Kuy </title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
 <head>
-<div class="main">
-    <img src="logo.png" alt="logo" style="width:250px;height:auto;">
-
-    <
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Site Title -->
+    <title>Sehat Kuy</title>
+    <!-- Meta Description Tag -->
+    <meta name="Description" content="Sehat Kuy">
+    <!-- Favicon Icon -->
+    <link rel="icon" type="image/x-icon" href="images/favicon.png" />
+    <!-- Font Awesoeme Stylesheet CSS -->
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" />
+    <!-- Google web Font -->
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600">
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Material Design Lite Stylesheet CSS -->
+    <link rel="stylesheet" href="css/material.min.css" />
+    <!-- Material Design Select Field Stylesheet CSS -->
+    <link rel="stylesheet" href="css/mdl-selectfield.min.css">
+    <!-- Owl Carousel Stylesheet CSS -->
+    <link rel="stylesheet" href="css/owl.carousel.min.css" />
+    <!-- Animate Stylesheet CSS -->
+    <link rel="stylesheet" href="css/animate.min.css" />
+    <!-- Magnific Popup Stylesheet CSS -->
+    <link rel="stylesheet" href="css/magnific-popup.css" />
+    <!-- Flex Slider Stylesheet CSS -->
+    <link rel="stylesheet" href="css/flexslider.css" />
+    <!-- Custom Main Stylesheet CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/sehatkuy.css">
 </head>
 <body style="background-color: #f9dbf0">
-    <ul>
-  <li><a href="/codeigniter">Home</a></li>
+<body>
+    
+    <header id="header">
+        
+        <div id="hdr-wrapper">
+            <div class="layer-stretch hdr">
+                <div class="tbl">
+                    <div class="tbl-row">
+                        
+                        <div class="tbl-cell hdr-logo">
+                            <a href="index.html"><img src="images/logo.png" alt=""></a>
+                        </div>
+                        <div class="tbl-cell hdr-menu">
+                            
+                            <ul class="menu">
+<li><a href="/codeigniter">Home</a></li>
   <li><a href="/codeigniter/obat">Data Obat</a></li>
   <li><a href="/codeigniter/sehat">Hidup Sehat</a></li>
   <li><a href="/codeigniter/penyakit">Data Penyakit</a></li>
   <li><a href="/codeigniter/lokasi">Lokasi Rumah Sakit</a></li>
   <li><a class="active" href="/codeigniter/tentang">Tentang</a></li>
-</ul>
+                            
+                    </ul>
+                       </div>
+            </div>
+        </div>
+    </header>           
+
     
-<br>
-	<h2>Ketua Kelompok</h2>
-
-	<br><br>
-	<div class="batas container">
-		<img src="<?php echo site_url('images/Faizal.jpg'); ?>" alt="Faizal" style="width:600px;height:400px">
-		<div class="text-block">
-    <h4>Faizal Alif</h4>
-    <p>14116032</p>
-    	</div>
+    <div class="page-ttl">
+        <div class="layer-stretch">
+            <div class="page-ttl-container">
+                <h1>Tentang Kami</h1>
+                <p><a href="/codeigniter">Home</a> &#8594; <span>Tentang Kami</span></p>
+            </div>
+        </div>
     </div>
 
-	<h2>Anggota Kelompok</h2>
-	
-	<br><br>
-	<div class="batas container">
-		<img src="<?php echo site_url('images/Putra_K.jpg'); ?>" alt="Putra" style="width:600px;height:400px">
-		<div class="text-block">
-    <h4>Putra Kurniawan</h4>
-    <p>14116026</p>
-    	</div>
+    <div class="layer-stretch">
+        <div class="layer-wrapper">
+            <div class="layer-container row">
+                <div class="col-md-5 hm-service-left">
+                    <img class="" src="images/logo.png" alt="">
+                </div>
+                <div class="col-md-7 hm-service-right">
+                    <p class="paragraph-medium paragraph-black">
+                        Sumber informasi untuk topik kesehatan,penyakit,obat,lokasi rumah sakit.
+                    </p>
+                    <p class="paragraph-medium paragraph-black">
+                       Sehat Kuy bertujuan untuk menjadi penyedia informasi kesehatan yang memungkinkan semua orang mendapat akses yang lebih baik sekaligus menginspirasi masyarakat untuk hidup lebih sehat dan bahagia.
+                    </p>
+                    <p class="paragraph-medium paragraph-black">
+                       Sehat Kuy mempunyai misi menginspirasi dan membantu mewujudkan hidup yang lebih sehat. 
+                    </p>
+<br><br><br><br>
+                </div>
+            </div>
+        </div>
     </div>
+   
+        <div class="layer-stretch">
+            <div class="layer-wrapper">
+                <div class="layer-ttl layer-ttl-white">
+                    <h3>Our Team</h3>
+                </div>
+                <div class="layer-container">
+                    <div id="hm-doctor-slider" class="owl-carousel owl-theme theme-owl-dot">
+                        <div class="hm-doctor">
+                            <img class="img-responsive" src="images/Faizal.jpg" alt="">
+                            <h6>Faizal Alif</h6>
+                            <p>14116054</p>
+                        </div>
+                        <div class="hm-doctor">
+                            <img class="img-responsive" src="images/Faiz.jpg" alt="" >
+                            <h6>Bahrul Faizi </h6>
+                            <p>14116037</p>
+                        </div>
+                        <div class="hm-doctor">
+                            <img class="img-responsive" src="images/Osa.jpg" alt="">
+                            <h6>Osa Farida Sari</h6>
+                            <p>14116054</p>
+                        </div>
+                        <div class="hm-doctor">
+                            <img class="img-responsive" src="images/Angela.jpg" alt="">
+                            <h6>Angela Merici L.R.D</h6>
+                            <p>14116035</p>
+                        </div>
+                        <div class="hm-doctor">
+                            <img class="img-responsive" src="images/Putra_K.jpg" alt="">
+                            <h6>Putra Kurniawan</h6>
+                            <p>14116026</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+     
+    
+  
 
-    <br><br>
-	<div class="batas container">
-		<img src="<?php echo site_url('images/Angela.jpg'); ?>" alt="Angela" style="width:600px;height:400px">
-		<div class="text-block">
-    <h4>Angela Merici L. R. D.</h4>
-    <p>14116035</p>
-    	</div>
-    </div>
+    <!-- **********Included Scripts*********** -->
 
-	<br><br>
-	<div class="batas container">
-		<img src="<?php echo site_url('images/Faiz.jpg'); ?>" alt="Faiz" style="width:600px;height:400px">
-		<div class="text-block">
-    <h4>Bahrul Faizi</h4>
-    <p>14116037</p>
-    	</div>
-    </div>
+    <!-- Jquery Library 2.1 JavaScript-->
+    <script src="js/jquery-2.1.4.min.js"></script>
+    <!-- Popper JavaScript-->
+    <script src="js/popper.min.js"></script>
+    <!-- Bootstrap Core JavaScript-->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Material Design Lite JavaScript-->
+    <script src="js/material.min.js"></script>
+    <!-- Material Select Field Script -->
+    <script src="js/mdl-selectfield.min.js"></script>
+    <!-- Flexslider Plugin JavaScript-->
+    <script src="js/jquery.flexslider.min.js"></script>
+    <!-- Owl Carousel Plugin JavaScript-->
+    <script src="js/owl.carousel.min.js"></script>
+    <!-- Scrolltofixed Plugin JavaScript-->
+    <script src="js/jquery-scrolltofixed.min.js"></script>
+    <!-- Magnific Popup Plugin JavaScript-->
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <!-- WayPoint Plugin JavaScript-->
+    <script src="js/jquery.waypoints.min.js"></script>
+    <!-- CounterUp Plugin JavaScript-->
+    <script src="js/jquery.counterup.js"></script>
+    <!-- SmoothScroll Plugin JavaScript-->
+    <script src="js/smoothscroll.min.js"></script>
+    <!--Custom JavaScript for Klinik Template-->
+    <script src="js/custom.js"></script>
+    <!-- Google Map Block Script -->
+    <script>
+        var map;
+        function initMap() {
+            var loc = {
+                lat: YOUR_LATTITUDE,
+                lng: YOUR_LONGITUDE 
+            };
+            var isDraggable = !('ontouchstart' in document.documentElement);
 
-    <br><br>
-	<div class="batas container">
-		<img src="<?php echo site_url('images/Osa.jpg'); ?>" alt="Osa" style="width:600px;height:400px">
-		<div class="text-block">
-    <h4>Osa Farida Sari</h4>
-    <p>14116054</p>
-    	</div>
-    </div>
+            map = new google.maps.Map(document.getElementById('map-container'), {
+                center: loc,
+                zoom: 14,
+                draggable: isDraggable,
+                scrollwheel: false
+            });
 
-
+            var marker = new google.maps.Marker({
+                position: loc,
+                map: map
+            });
+        }
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&amp;callback=initMap" async defer></script>
 </body>
-
 </html>
