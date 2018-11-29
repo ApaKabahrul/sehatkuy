aabace4b015ee33a57449d1e1bafd61207ba5b18
Angelamerici <38027594+Angelamerici@users.noreply.github.com>
2018-11-29 15:06:29 +0700

    Add files via upload

---
+++ isi.php
@@ -0,0 +1,133 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Site Title -->
+    <title>Sehat Kuy</title>
+    <!-- Meta Description Tag -->
+    <meta name="Description" content="Sehat Kuy">
+    <!-- Favicon Icon -->
+    <link rel="icon" type="image/x-icon" href="images/favicon.png" />
+    <!-- Font Awesoeme Stylesheet CSS -->
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" />
+    <!-- Google web Font -->
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600">
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Material Design Lite Stylesheet CSS -->
+    <link rel="stylesheet" href="css/material.min.css" />
+    <!-- Material Design Select Field Stylesheet CSS -->
+    <link rel="stylesheet" href="css/mdl-selectfield.min.css">
+    <!-- Owl Carousel Stylesheet CSS -->
+    <link rel="stylesheet" href="css/owl.carousel.min.css" />
+    <!-- Animate Stylesheet CSS -->
+    <link rel="stylesheet" href="css/animate.min.css" />
+    <!-- Magnific Popup Stylesheet CSS -->
+    <link rel="stylesheet" href="css/magnific-popup.css" />
+    <!-- Flex Slider Stylesheet CSS -->
+    <link rel="stylesheet" href="css/flexslider.css" />
+    <!-- Custom Main Stylesheet CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/sehatkuy.css">
+</head>
+<body style="background-color: #f9dbf0">
+  
+<header id="header-3">
+         <div class="layer-stretch hdr-center">
+            <div class="row align-items-center">
+                <div class="col-md-5 hidden-xs">    
+                </div>
+                <div class="col-md-2">
+                    <div class="hdr-center-logo text-center">
+                        <a href="index.html" class="d-inline-block"><img src="images/logo.png" alt=""></a>
+                    </div>
+                </div>
+        
+ </div>
+        </div>
+        <div class="hdr-center-menu">
+            <div class="hdr layer-stretch">
+                <div class="row align-items-center justify-content-end">
+                   
+                    <ul class="col menu text-left">
+  <li><a class="active" href="/codeigniter">Home</a></li>
+  <li><a href="/codeigniter/obat">Data Obat</a></li>
+  <li><a href="/codeigniter/sehat">Hidup Sehat</a></li>
+  <li><a href="/codeigniter/penyakit">Data Penyakit</a></li>
+  <li><a href="/codeigniter/lokasi">Lokasi Rumah Sakit</a></li>
+  <li><a href="/codeigniter/tentang">Tentang</a></li>
+                            
+                    </ul>
+
+                </div>
+                
+            </div>
+        </div>
+    </header>           
+
+
+  <div id="hm-blog" class="layer-stretch">
+        <div class="layer-wrapper layer-bottom-10 text-center">
+            <div class="layer-ttl">
+<br><br><br><br><br><br><br><br><br><br><br><br>
+                    <h3>Welcome To Our Website</h3>
+                    
+                </div>
+                
+                        <div class="hm-about-paragraph">
+                            <h3>
+                                <span class="theme-dropcap color-black"></span>
+                                Your Health.Our Mission.
+                            </h3> 
+                            <br><br><br><br><br><br><br><br><br><br><br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- **********Included Scripts*********** -->
+
+    <!-- Jquery Library 2.1 JavaScript-->
+    <script src="js/jquery-2.1.4.min.js"></script>
+    <!-- Popper JavaScript-->
+    <script src="js/popper.min.js"></script>
+    <!-- Bootstrap Core JavaScript-->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Material Design Lite JavaScript-->
+    <script src="js/material.min.js"></script>
+    <!-- Material Select Field Script -->
+    <script src="js/mdl-selectfield.min.js"></script>
+    <!-- Flexslider Plugin JavaScript-->
+    <script src="js/jquery.flexslider.min.js"></script>
+    <!-- Owl Carousel Plugin JavaScript-->
+    <script src="js/owl.carousel.min.js"></script>
+    <!-- Scrolltofixed Plugin JavaScript-->
+    <script src="js/jquery-scrolltofixed.min.js"></script>
+    <!-- Magnific Popup Plugin JavaScript-->
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <!-- WayPoint Plugin JavaScript-->
+    <script src="js/jquery.waypoints.min.js"></script>
+    <!-- CounterUp Plugin JavaScript-->
+    <script src="js/jquery.counterup.js"></script>
+    <!-- SmoothScroll Plugin JavaScript-->
+    <script src="js/smoothscroll.min.js"></script>
+    <!--Custom JavaScript for Klinik Template-->
+    <script src="js/custom.js"></script>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-93901876-1', 'auto');
+        ga('send', 'pageview');
+    </script>
+</body>
+</html>
